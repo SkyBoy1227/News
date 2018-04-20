fcc44c314502f5afa9f0c0daa4a85214ea476b1c
ASUS <553728561@qq.com>
2018-04-20 17:01:33 +0800

    主页面正文Fragment的布局

--- app/src/main/java/com/sky/app/news/fragment/ContentFragment.java
+++ app/src/main/java/com/sky/app/news/fragment/ContentFragment.java
@@ -1,12 +1,15 @@
 package com.sky.app.news.fragment;
 
-import android.graphics.Color;
-import android.view.Gravity;
+import android.support.v4.view.ViewPager;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.RadioGroup;
 
+import com.sky.app.news.R;
 import com.sky.app.news.base.BaseFragment;
 import com.sky.app.news.utils.LogUtil;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created with Android Studio.
@@ -18,22 +21,24 @@ import com.sky.app.news.utils.LogUtil;
  * @version ${VERSION}
  */
 public class ContentFragment extends BaseFragment {
-    private TextView textView;
+
+    @BindView(R.id.view_pager)
+    ViewPager viewPager;
+    @BindView(R.id.rg_bottom)
+    RadioGroup rgBottom;
 
     @Override
     protected View initView() {
         LogUtil.e("正文Fragment视图被初始化了");
-        textView = new TextView(context);
-        textView.setTextSize(26);
-        textView.setTextColor(Color.RED);
-        textView.setGravity(Gravity.CENTER);
-        return textView;
+        View view = View.inflate(context, R.layout.fragment_content, null);
+        ButterKnife.bind(this, view);
+        return view;
     }
 
     @Override
     public void initData() {
         super.initData();
         LogUtil.e("正文Fragment数据被初始化了");
-        textView.setText("正文Fragment页面");
+        rgBottom.check(R.id.rb_home);
     }
 }
