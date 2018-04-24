98c70eae7cd15069b2743425f4f01e2de1be6fb5
ASUS <553728561@qq.com>
2018-04-24 16:14:20 +0800

    配置网络请求数据

---
+++ app/src/main/java/com/sky/app/news/utils/Constants.java
@@ -0,0 +1,27 @@
+package com.sky.app.news.utils;
+
+/**
+ * Created with Android Studio.
+ * 描述: 常量类，配置联网请求地址
+ * Date: 2018/4/24
+ * Time: 16:10
+ *
+ * @author 晏琦云
+ * @version ${VERSION}
+ */
+public class Constants {
+    /**
+     * ip地址
+     */
+    public static final String HOST = "192.168.56.1";
+
+    /**
+     * 联网请求服务器的基本地址
+     */
+    public static final String BASE_URL = "http://" + HOST + ":8080/web_home";
+
+    /**
+     * 新闻中心的网络地址
+     */
+    public static final String NEWSCENTER_PAGER_URL = BASE_URL + "/static/api/news/categories.json";
+}
