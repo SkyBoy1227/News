c87f848b911ec8a5876355bb407c4bfd14ef3551
ASUS <553728561@qq.com>
2018-04-27 17:12:40 +0800

    页签详情数据访问和解析

--- app/src/main/java/com/sky/app/news/menudetailpager/tabdetailpager/TabDetailPager.java
+++ app/src/main/java/com/sky/app/news/menudetailpager/tabdetailpager/TabDetailPager.java
@@ -1,13 +1,21 @@
 package com.sky.app.news.menudetailpager.tabdetailpager;
 
 import android.content.Context;
+import android.text.TextUtils;
 import android.view.View;
 
+import com.google.gson.Gson;
 import com.sky.app.news.R;
 import com.sky.app.news.base.MenuDetailBasePager;
 import com.sky.app.news.domain.NewsCenterPagerBean2;
+import com.sky.app.news.domain.TabDetailPagerBean;
+import com.sky.app.news.utils.CacheUtils;
 import com.sky.app.news.utils.Constants;
 import com.sky.app.news.utils.LogUtil;
+
+import org.xutils.common.Callback;
+import org.xutils.http.RequestParams;
+import org.xutils.x;
 
 /**
  * Created with Android Studio.
@@ -39,5 +47,61 @@ public class TabDetailPager extends MenuDetailBasePager {
         super.initData();
         url = Constants.BASE_URL + childrenData.getUrl();
         LogUtil.e(childrenData.getTitle() + "的联网地址：" + url);
+        // 得到缓存的文本数据
+        String cacheJson = CacheUtils.getString(context, url);
+        if (!TextUtils.isEmpty(cacheJson)) {
+            processData(cacheJson);
+        }
+        getDataFromNet();
+    }
+
+    /**
+     * 解析json数据并且显示数据
+     *
+     * @param json
+     */
+    private void processData(String json) {
+        TabDetailPagerBean bean = parsedJson(json);
+        LogUtil.e(childrenData.getTitle() + "解析成功==" + bean.getData().getNews().get(0).getTitle());
+    }
+
+    /**
+     * 解析json数据
+     *
+     * @param json
+     * @return
+     */
+    private TabDetailPagerBean parsedJson(String json) {
+        return new Gson().fromJson(json, TabDetailPagerBean.class);
+    }
+
+    /**
+     * 联网请求数据
+     */
+    private void getDataFromNet() {
+        RequestParams params = new RequestParams(url);
+        x.http().get(params, new Callback.CommonCallback<String>() {
+            @Override
+            public void onSuccess(String result) {
+                // 缓存数据
+                CacheUtils.putString(context, url, result);
+                LogUtil.e(childrenData.getTitle() + "-页面数据请求成功==" + result);
+            }
+
+            @Override
+            public void onError(Throwable ex, boolean isOnCallback) {
+                LogUtil.e(childrenData.getTitle() + "-页面数据请求失败==" + ex.getMessage());
+            }
+
+            @Override
+            public void onCancelled(CancelledException cex) {
+                LogUtil.e(childrenData.getTitle() + "-onCancelled==" + cex.getMessage());
+            }
+
+            @Override
+            public void onFinished() {
+                LogUtil.e(childrenData.getTitle() + "-onFinished");
+            }
+        });
     }
 }
