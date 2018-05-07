1fe49da3b8e11064a0fdb81a4a9fda7057880d17
ASUS <553728561@qq.com>
2018-05-07 11:50:07 +0800

    WebView字体大小修改

--- app/src/main/java/com/sky/app/news/activity/NewsDetailActivity.java
+++ app/src/main/java/com/sky/app/news/activity/NewsDetailActivity.java
@@ -2,6 +2,7 @@ package com.sky.app.news.activity;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.webkit.WebSettings;
@@ -41,6 +42,9 @@ public class NewsDetailActivity extends AppCompatActivity {
     @BindView(R.id.pb_loading)
     ProgressBar pbLoading;
     private String url;
+    private int tempSize = 2;
+    private int realSize = tempSize;
+    private WebSettings settings;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -54,13 +58,15 @@ public class NewsDetailActivity extends AppCompatActivity {
     private void getData() {
         url = getIntent().getStringExtra("url");
 
-        WebSettings settings = webview.getSettings();
+        settings = webview.getSettings();
         // 设置支持javaScript
         settings.setJavaScriptEnabled(true);
         // 设置双击变大变小
         settings.setUseWideViewPort(true);
         // 增加缩放按钮
         settings.setBuiltInZoomControls(true);
+        // 改变字体大小
+        settings.setTextZoom(100);
         // 不让当前网页跳转到系统的浏览器中
         webview.setWebViewClient(new WebViewClient() {
             // 当加载页面完成的时候回调
@@ -87,10 +93,58 @@ public class NewsDetailActivity extends AppCompatActivity {
                 finish();
                 break;
             case R.id.ib_textsize:
-                Toast.makeText(this, "设置字体大小", Toast.LENGTH_SHORT).show();
+                showChangeTextSizeDialog();
                 break;
             case R.id.ib_share:
                 Toast.makeText(this, "分享", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }
+    }
+
+    /**
+     * 显示改变字体大小的对话框
+     */
+    private void showChangeTextSizeDialog() {
+        String[] items = {"超大字体", "大字体", "正常字体", "小字体", "超小字体"};
+        new AlertDialog.Builder(this)
+                .setTitle("设置字体大小")
+                .setSingleChoiceItems(items, realSize, (dialog, which) -> tempSize = which)
+                .setNegativeButton("取消", null)
+                .setPositiveButton("确定", (dialog, which) -> {
+                    realSize = tempSize;
+                    changeTextSize(realSize);
+                })
+                .show();
+    }
+
+    /**
+     * 改变字体大小
+     *
+     * @param textSize
+     */
+    private void changeTextSize(int textSize) {
+        switch (textSize) {
+            case 0:
+                // 超大字体
+                settings.setTextZoom(200);
+                break;
+            case 1:
+                // 大字体
+                settings.setTextZoom(150);
+                break;
+            case 2:
+                // 正常字体
+                settings.setTextZoom(100);
+                break;
+            case 3:
+                // 小字体
+                settings.setTextZoom(75);
+                break;
+            case 4:
+                // 超小字体
+                settings.setTextZoom(50);
                 break;
             default:
                 break;
